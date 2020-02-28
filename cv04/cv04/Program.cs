d313e86a51f20862a70740b64be58ae01f598972
Student <Student@PC-SE5113-10.utko.feec.vutbr.cz>
2020-02-28 14:14:48 +0100

    End of Lesson cv04, points: 2b

---
+++ cv04/cv04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringtestovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+                +"ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                + "posledni veta!";
+
+            string stringtestovaciTextInficated = "Toto je retezec predstavovany nekolika radky,\n"
+                + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                + "koronavirus posledni veta!";
+
+            StringStatistics a = new StringStatistics(stringtestovaciText);
+            StringStatistics b = new StringStatistics(stringtestovaciTextInficated);
+
+            test(a);
+            Console.WriteLine("------------------------------------------------");
+            test(b);
+            Console.ReadLine();
+        }
+
+        public static void test(StringStatistics a) {
+
+            Console.WriteLine("Pocet slov: {0}", a.SumWord);
+            Console.WriteLine("Pocet radku: {0}", a.SumRows);
+            Console.WriteLine("Pocet vet: {0}", a.SumSentence);
+            Console.WriteLine("Nejdelší slova: {0}", a.LongestWord);
+            Console.WriteLine("Nejkratsi slova: {0}", a.ShortestWord);
+            Console.WriteLine("Slova podle abecedy: {0}", a.AlfabetWord);
+            if (a.Inficeted)
+            {
+                Console.WriteLine("POZOR VETA JE INFIKOVANA!!!!!!!!");
+            }
+            
+        }
+    }
+}
---
+++ cv04/cv04/StringStatistics.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv04
+{
+    class StringStatistics
+    {
+        private string inputString;
+        private int sumWord = 0;
+        private int sumRows = 0;
+        private int sumSentence = 0;
+        private List<string> longestWord = new List<string>();
+        private int longestWordSize = 0;
+        private List<string> shortestWord = new List<string>();
+        private int shortestWordSize = int.MaxValue;
+        private string[] alfabetWord;
+        private bool inficeted = false;
+
+        public StringStatistics(string inputString)
+        {
+            this.inputString = inputString;
+            char[] separators;
+            string[] list;
+
+            // calcule sum lines
+            separators = new char[] { '\n' };
+            list = inputString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            sumRows = list.Length;
+
+            // calcule sum senteces
+            separators = new char[] { '.', '!', '?' };
+            list = inputString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if ((i + 1) < list.Length)
+                {
+                    //Console.WriteLine(list[i + 1].ToCharArray()[1]);
+                    if (Char.IsUpper(list[i + 1].ToCharArray()[1]))
+                    {
+                        sumSentence++;
+                    }
+                }
+                else
+                {
+                    sumSentence++;
+                }
+            }
+
+            // calcule sum words
+            separators = new char[] { ' ', '.', ',', ';', '!', '?', '\n', '(', ')' };
+            list = inputString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            sumWord = list.Length;
+
+            // find longist word and shortest
+            foreach (string item in list)
+            {
+                if (longestWordSize == item.Length)
+                {
+                    longestWord.Add(item);
+                }
+                if (longestWordSize < item.Length)
+                {
+                    longestWord = new List<string>();
+                    longestWord.Add(item);
+                    longestWordSize = item.Length;
+                }
+
+
+
+                if (shortestWordSize == item.Length)
+                {
+                    shortestWord.Add(item);
+                }
+                if (shortestWordSize > item.Length)
+                {
+                    shortestWord = new List<string>();
+                    shortestWord.Add(item);
+                    shortestWordSize = item.Length;
+                }
+
+            }
+
+            Array.Sort(list);
+            alfabetWord = list;
+
+            inficeted = inputString.Contains("koronavirus");
+
+
+           
+
+
+        }
+ 
+        public int SumWord { get { return sumWord; } }
+        public int SumRows { get { return sumRows; } }
+        public int SumSentence { get { return sumSentence; } }
+        public bool Inficeted { get { return inficeted; } }
+
+        public string LongestWord {
+            get {
+                StringBuilder buffer = new StringBuilder("");
+                foreach (string item in longestWord)
+                {
+                    buffer.Append(item);
+                }
+                return buffer.ToString();
+            }
+        }
+
+        public string ShortestWord
+        {
+            get
+            {
+                StringBuilder buffer = new StringBuilder("");
+                foreach (string item in shortestWord)
+                {
+                    buffer.Append(item);
+                    buffer.Append(" ");
+                }
+                return buffer.ToString();
+            }
+        }
+
+        public string AlfabetWord
+        {
+            get
+            {
+                StringBuilder buffer = new StringBuilder("");
+                for (int i = 0; i < alfabetWord.Length; i++)
+                {
+                    if (i != 0) {
+                        buffer.Append(" ,");
+                    }
+                    buffer.Append(alfabetWord[i]);
+                }
+                return buffer.ToString();
+            }
+        }
+    }
+
+}
+
+
+
+
+    
+
+   
+
+
+
+
